ba75d458aae885eb26f7c6cfbe365cf6df4e641d
somecallmetim <timbauer@ymail.com>
2018-10-04 18:27:52 -0700

    minor refactor for readability

--- scripts.js
+++ scripts.js
@@ -16,19 +16,31 @@ function flipCard(){
         hasFlippedCard = false;
         secondCard = this;
 
-        // do cards match?
-        if(firstCard.dataset.cardname === secondCard.dataset.cardname){
-            // it's a match
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        }else {
-            // not a match
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-        }
+        checkForMatch();
     }
+}
+
+function checkForMatch(){
+    // do cards match?
+    if(firstCard.dataset.cardname === secondCard.dataset.cardname){
+        // it's a match
+        disableCards();
+    }else {
+        // not a match
+        unflipCards();
+    }
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
